909c806abb43b99cde408361a6517ebc7cdea44e
medaldev <webformedal@gmail.com>
2019-05-02 23:38:00 +0300

    Настрока вывода главной страницы.

--- mdlblog/blog/views.py
+++ mdlblog/blog/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import FormView, TemplateView, View, CreateView, UpdateView, DeleteView
 
-# Create your views here.
+"""class MainView(TemplateView):
+    template_name = 'index.html'
+
+    def get(self, request):
+        return render(request, self.template_name, {})
+"""
+
+
+def home(request):
+    return render(request, "partial/index.html")
+
